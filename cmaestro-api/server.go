dd0d0621c8c75288f451e8274e35e80293602e66
qerdinger <91551241+QuentinCraft@users.noreply.github.com>
2026-05-14 13:46:07 +0200

    docs: adds default routes (sys/health/status)

--- cmaestro-api/server.go
+++ cmaestro-api/server.go
@@ -11,12 +11,15 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/users", getUsers)
+	// system
+	// health
+	// status
 
 	http.ListenAndServe(":8080", r)
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
-	users := []string{"Alice", "Bob"}
+	users := []string{"admin"}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(users)
